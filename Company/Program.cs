805cf1ed246eeb82f8d439a252c797c32ec656a4
Erik Parso <parsoerik@gmail.com>
2022-06-24 15:10:43 +0200

    Program implementation

--- Company/Program.cs
+++ Company/Program.cs
@@ -1,14 +1,55 @@
 ﻿using Company;
 var kros = new Company.Company("Blaznec s.r.o.");
+var counter = 0;
 
-var teacher1 = new Teacher(1, "Peter", new DateOnly(2022, 1, 1), 500);
-var teacher2 = new Teacher(2, "Jana", new DateOnly(2022, 2, 1), 450);
-var manager = new Manager(3, "Fero", new DateOnly(2021, 9, 1), 800, true, true, false);
-kros.AddEmployee(teacher1);
-kros.AddEmployee(teacher2);
-kros.AddEmployee(manager);
-
-foreach (var item in kros.Employees)
+while (true)
 {
-    Console.WriteLine(item.GetInfo());
+	Console.WriteLine("Action: a-add, s-show, e-exit");
+	var action = Console.ReadKey().KeyChar;
+	Console.WriteLine();
+
+	if (action == 'a')
+	{
+		Console.WriteLine("Name:");
+		var name = Console.ReadLine() ?? "John Doe";
+
+		Console.WriteLine("Rate:");
+		var rateString = Console.ReadLine() ?? "0";
+		var rate = int.Parse(rateString);
+
+		Console.WriteLine("Role: t-teacher, m-manager");
+		var role = Console.ReadKey().KeyChar;
+		Console.WriteLine();
+
+		switch (role)
+		{
+			case 't':
+				var teacher = new Teacher(counter++, name, new DateOnly(), rate);
+				kros.AddEmployee(teacher);
+				break;
+			case 'm':
+				var manager = new Manager(counter++, name, new DateOnly(), rate, false, false, false);
+				kros.AddEmployee(manager);
+				break;
+			default:
+				throw new NotImplementedException("Role " + role + " is not implemented");
+		}
+	}
+	else if (action == 's')
+	{	
+		Console.WriteLine("-------------------------");
+		foreach (var employee in kros.Employees)
+		{
+			Console.WriteLine(employee.GetInfo());
+			Console.WriteLine("-------------------------");
+		}
+	}
+	else if (action == 'e')
+	{
+		break;
+	}
+	else
+	{
+		throw new NotImplementedException("Action " + action + " is not implemented");
+	}
 }
