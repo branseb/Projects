81945da1a519896db92daff87aba7aa1b6ed4c0e
branseb@gmail.com <branseb@gmail.com>
2022-07-26 13:13:41 +0200

    add montecarlo aplication

---
+++ Montecarlo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+var generator = new Random(0);
+var generatory = new Random(1);
+
+List<(double, double,bool)> points = new List<(double, double, bool)>();
+
+int inCircleCount=0;
+int allPoints = 0;
+double priemer = 0;
+int count = -1;
+while (true)
+{
+    count++;
+    while (count < 101)
+    {
+        double x = generator.NextDouble();
+        double y = generatory.NextDouble();
+        double diffX = 0.5 - x;
+        double diffY = 0.5 - y;
+        double fromMiddle = Math.Sqrt(Math.Pow(diffX, 2.0) + Math.Pow(diffY, 2));
+        bool inCircle = fromMiddle <= 0.5;
+
+        if (inCircle)
+        {
+            inCircleCount++;
+        }
+
+        allPoints++;
+
+        priemer = priemer + ((double)inCircleCount / allPoints * 4);
+
+        count++;
+    }
+    double priemertotal = priemer / count;
+    Console.WriteLine(priemertotal);
+}
