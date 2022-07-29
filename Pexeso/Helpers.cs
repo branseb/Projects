9153433ddc66ec72f9d9606a7e5cc4f4e226ffe3
branseb@gmail.com <branseb@gmail.com>
2022-07-29 15:03:31 +0200

    pexeso beta

---
+++ Pexeso/Helpers.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+namespace Pexeso
+{
+    public static class Helpers
+    {
+        public static Stack<char> GetCards(int countcards)
+        {
+            List<char> cards = new List<char>();
+            for (int i = 65; i < countcards / 2 + 65; i++)
+            {
+                cards.Add((char)i);
+                cards.Add((char)i);
+            }
+            var random = new Random();
+            cards = cards.OrderBy(x => random.Next()).ToList();
+            return new Stack<char>(cards);
+        }
+        public static char[,] GetArray(int x, int y)
+        {
+            var count = x * y;
+            var deck = Helpers.GetCards(count);
+            var array = new char[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    array[i, j] = deck.Pop();
+                }
+
+            }
+            return array;
+        }
+        public static bool TryParseSize(string xstring, string ystring, out int x, out int y)
+        {
+
+
+            bool xbool = int.TryParse(xstring, out x);
+            bool ybool = int.TryParse(ystring, out y);
+
+            return ((x * y) % 2 == 0 && xbool && ybool);
+
+        }
+        public static bool GetCoordinate(string coordinateXY, char [,] cards,out int x, out int y)
+        {
+          
+            bool xbool = int.TryParse(coordinateXY[0].ToString(), out x);
+            bool ybool = int.TryParse(coordinateXY[1].ToString(), out y);
+            if (coordinateXY.Length != 2 || !xbool || !ybool)
+            {
+                return false;
+            }
+
+            var xOk = 0 < x && x <= cards.GetLength(0);
+            var yOk = y > 0 && y <= cards.GetLength(1);
+
+            
+            if (!xOk || !yOk || cards [x,y] == ' ')
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static string GetGraphic(char[,] cards, int card1x, int card1y, int card2x, int card2y)
+        {
+            StringBuilder graphic = new StringBuilder();
+            for (int i = 0; i < cards.GetLength(0); i++)
+            {
+                for (int j = 0; j < cards.GetLength(1); j++)
+                {
+                    if ((i == card1x-1 && j == card1y-1)||(i == card2x-1 && j == card2y-1))
+                    {
+                        graphic.Append(cards[i,j]+"|");
+                    }
+                    else if (cards[i,j] == ' ')
+                    {
+                        graphic.Append(" |");
+                    }
+                    else
+                    {
+                        graphic.Append("░|");
+                    }
+
+                }
+                graphic.AppendLine();
+            }
+            return graphic.ToString();
+        }
+    }
+}
---
+++ Pexeso/Program.cs
@@ -0,0 +1,110 @@
+﻿using Pexeso;
+
+Console.WriteLine("Hi!");
+bool sizebool = false;
+int sizex = 0;
+int sizey = 0;
+while (!sizebool)
+{
+    Console.WriteLine("Choose first size:");
+    string sizexstring = Console.ReadLine();
+
+    Console.WriteLine("Choose second size:");
+    string sizeystring = Console.ReadLine();
+    sizebool = Helpers.TryParseSize(sizexstring, sizeystring, out sizex, out sizey);
+    if (!sizebool)
+    {
+        Console.WriteLine("Wrong choice!!");
+    }
+
+}
+char[,] cards = Helpers.GetArray(sizex, sizey);
+bool end = true;
+(int id, int score) player1 = (1, 0);
+(int id, int score) player2 = (2, 0);
+(int id, int score) player = (player1.id, player1.score);
+while (end)
+{
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 0;
+    int y2 = 0;
+    Console.WriteLine("Get Player Nr.{0}", player.id);
+    bool firstcard = false;
+    while (!firstcard)
+    {
+        Console.WriteLine("Enter coordinate xy first cards:");
+        var coordinatexy = Console.ReadLine();
+        firstcard = Helpers.GetCoordinate(coordinatexy, cards, out x1, out y1);
+        if (!firstcard)
+        {
+            Console.WriteLine("Bad choice! Try Again");
+        }
+    }
+    bool seconcard = false;
+    while (!seconcard)
+    {
+        Console.WriteLine("Enter coordinate xy second cards:");
+        var coordinatexy = Console.ReadLine();
+        seconcard = Helpers.GetCoordinate(coordinatexy, cards, out x2, out y2);
+        if (!seconcard)
+        {
+            Console.WriteLine("Bad choice! Try Again");
+        }
+    }
+    Console.WriteLine(Helpers.GetGraphic(cards,x1,y1,x2,y2));
+    if (cards[x1, y1] == cards[x2, y2])
+    {
+        Console.WriteLine("Match!");
+        cards[x1, y1] = ' ';
+        cards[x2, y2] = ' ';
+        if (player.id == 1)
+        {
+            player1.score++;
+        }
+        else
+        {
+            player2.score++;
+
+        }
+    }
+    else
+    {
+        Console.WriteLine("Bad Luck!");
+    }
+    if (player.id == player1.id)
+    {
+        player = player2;
+    }
+    else if (player.id == player2.id)
+    {
+        player = player1;
+    }
+    int endint = 0;
+    foreach (var card in cards)
+    {
+
+        if (card == ' ')
+        {
+            endint++;
+        }
+
+    }
+    if (endint == cards.Length)
+    { 
+        end = false;
+        if (player1.score>player2.score)
+        {
+            Console.WriteLine("Winner its Player Nr." + player1.id);
+        }
+        else if (player2.score>player1.score)
+        {
+            Console.WriteLine("Winner its Player Nr." + player2.id);
+        }
+        else
+        {
+            Console.WriteLine("No winner !");
+        }
+    }
+    
+}
