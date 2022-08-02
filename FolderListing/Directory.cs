b829e680b491eb30927c2b0b3d2fc05886709b4a
branseb@gmail.com <branseb@gmail.com>
2022-08-02 11:13:44 +0200

    add folder listing

---
+++ FolderListing/Directory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Extensions;
+
+namespace FolderListing
+{
+    public static class Directory
+    {
+        public static void RenderFolders(int level, string folder)
+        {
+            var directories = System.IO.Directory.GetDirectories(folder);
+            foreach (var d in directories)
+            {
+                Console.WriteLine(" ".multyply(level * 4) + d);
+                RenderFolders(level + 1, d);
+            }
+        }
+    }
+}
---
+++ FolderListing/Program.cs
@@ -0,0 +1,6 @@
+﻿using FolderListing;
+
+string route = "C:\\Users\\User\\source";
+FolderListing.Directory.RenderFolders(0, route);
+
+
